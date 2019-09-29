9e95602f020fbe7955e7372f645c187bd8abcd04
Regan A <36708505+indiansatoshi@users.noreply.github.com>
2019-09-29 22:53:04 +0530

    Add files via upload

---
+++ math.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.5.0;
+
+
+contract Math {
+
+    uint num1;
+
+    uint num2;
+
+
+    function getAdd (uint _num1, uint _num2) public view returns (uint) {
+
+       uint num3 = _num1 + _num2;
+
+        return num3;
+
+    }
+
+    function getDiff (uint _num1, uint _num2) public view returns (uint) {
+
+       uint num4 = _num1 - _num2;
+
+        return num4;
+
+    }
+
+    function getMul (uint _num1, uint _num2) public view returns (uint) {
+
+       uint num5 = _num1 * _num2;
+
+        return num5;
+
+    }
+
+    function getDiv (uint _num1, uint _num2) public view returns (uint) {
+
+       uint num6 = _num1 / _num2;
+
+        return num6;
+
+    }
+
+}

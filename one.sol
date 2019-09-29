9be74df4a8df97113150c0ef0e29a4e305ed1266
Regan A <36708505+indiansatoshi@users.noreply.github.com>
2019-09-29 22:36:25 +0530

    Add files via upload

---
+++ one.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract One
+{
+    string private stateVariable = "This is a sample solidity program";
+    function SampleOne() public view returns (string memory) {
+        return stateVariable;
+    }
+}

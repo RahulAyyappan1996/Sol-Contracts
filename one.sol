5b69d228e939f8e55d212e3bccf74bcfd3d53661
RahulAyyappan1996 <68462175+RahulAyyappan1996@users.noreply.github.com>
2020-09-06 16:21:25 +0530

    Update one.sol

--- one.sol
+++ one.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 contract One
 {
-    string private stateVariable = "This is a sample solidity program";
+    string private stateVariable = "This is sample solidity program";
     function SampleOne() public view returns (string memory) {
         return stateVariable;
     }
